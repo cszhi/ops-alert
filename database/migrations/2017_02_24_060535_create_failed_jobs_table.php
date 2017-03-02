1d12bd99f87e084af04061209be9e9f2e2b31847
csz <cszhi@qq.com>
2017-03-02 10:56:06 +0800

    failed_jobs migration

---
+++ database/migrations/2017_02_24_060535_create_failed_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFailedJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('connection');
+            $table->string('queue');
+            $table->longText('payload');
+            $table->unsignedInteger('failed_at')->nullable();
+     
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('failed_jobs');
+    }
+}
